2911dd5d574347a7eed6868802a397a2517395e0
Yeab243 <yeabsira.girma@seattlecolleges.edu>
2022-05-25 13:24:51 -0700

    domWalkthrough

---
+++ domWalkthrough.js
@@ -0,0 +1,9 @@
+// console.dir(document);
+
+// console.log(document.doamin); //<--Shows IP addreess
+
+// console.log(document.URL);
+
+console.log(document.title);
+console.log(document.doctype);
+console.log(head)
